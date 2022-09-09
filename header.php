c051995bcb7b6050793f736c6a9a1a3b96bfa6ce
TJ <teejay.recio@gmail.com>
2022-09-09 20:43:01 +0800

    0842pm

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
     <button id="buttonstyleheaderLeft" onclick="document.location='index.php'"><i class="fa-solid fa-house-chimney"></i>&nbsp;Home</button>
   </div>
   <div id="headerRight">
-  <button id="buttonstyleheaderRight" onclick="document.location='a.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Search Meds</button>
+    <button id="buttonstyleheaderRight" onclick="document.location='awstore.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Meds w/stores</button>
+    <button id="buttonstyleheaderRight" onclick="document.location='a.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Search Meds</button>
     <button id="buttonstyleheaderRight" onclick="document.location='login090622.php'"><i class="fa-solid fa-right-to-bracket"></i>&nbsp;Login</button>
     <button id="buttonstyleheaderRight" onclick="document.location='register090622.php'"><i class="fa-solid fa-pen-to-square"></i>&nbsp;Register</button>
   </div>
