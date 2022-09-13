d7289816ebd1151fcab53a83826c99c052540ff4
TJ <teejay.recio@gmail.com>
2022-09-13 16:39:57 +0800

    0439pm

--- headerloggedin.php
+++ headerloggedin.php
@@ -3,9 +3,7 @@
     <button id="buttonstyleheaderLeft" onclick="document.location='index.php'"><i class="fa-solid fa-house-chimney"></i>&nbsp;Home</button>
   </div>
   <div id="headerRight">
-  <button id="buttonstyleheaderRight" onclick="document.location='awstorewmap.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Stores w/ map</button>
-    <button id="buttonstyleheaderRight" onclick="document.location='awstore.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Meds w/stores</button>
-    <button id="buttonstyleheaderRight" onclick="document.location='a.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Search Meds</button>
+  <button id="buttonstyleheaderRight" onclick="document.location='awstorewmap.php'"><i class="fa-solid fa-file-prescription"></i>&nbsp;Search Meds</button>
     <button id="buttonstyleheaderRight" onclick="document.location='logout090622.php'"><i class="fa-solid fa-right-from-bracket"></i>&nbsp;Log out</button>
   </div>
 </div>
